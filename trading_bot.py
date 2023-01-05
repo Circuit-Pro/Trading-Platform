d71801482833c0c11fbb61dd48efdd5d710d45ba
CPI <oricalworks@gmail.com>
2023-01-05 01:10:55 -0500

    fixes and updates

--- trading_bot.py
+++ trading_bot.py
@@ -4,36 +4,46 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import tkinter as tk
 from tkinter import ttk
-from tkinter import Frame, Button, Entry, messagebox, Spinbox
+from tkinter import StringVar
+from tkinter import Frame, Button, Entry, Spinbox
 from tkinter import filedialog
+import tkinter.messagebox as messagebox
+
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Function to retrieve stock data and make predictions
 def predict_stock(ticker, period):
-    # Get stock data
-    stock_data = yf.Ticker(ticker).history(period=period)
+    try:
+        # Get stock data
+        stock_data = yf.Ticker(ticker).history(period=str(period))
+    except ValueError as e:
+        messagebox.showerror("Error", str(e))
+        return None
 
-    # Convert stock data to numpy array
-    X = np.array(range(len(stock_data))).reshape(-1, 1)
-    y = np.array(stock_data['Close']).reshape(-1, 1)
+        # Convert stock data to numpy array
+        X = np.array(range(len(stock_data))).reshape(-1, 1)
+        y = np.array(stock_data['Close']).reshape(-1, 1)
 
-    # Fit linear regression model
-    model = LinearRegression().fit(X, y)
+        # Fit linear regression model
+        model = LinearRegression().fit(X, y)
 
-    # Make predictions
-    predictions = model.predict(X)
+        # Make predictions
+        predictions = model.predict(X)
 
-    # Add predictions to stock data
-    stock_data['Prediction'] = predictions
+        # Add predictions to stock data
+        stock_data['Prediction'] = predictions
 
-    return stock_data
-
+        return stock_data
 
 # Function to create tab for stock
-def create_tab(ticker, percentage, period):
-    # Retrieve stock data and predictions
-    stock_data = predict_stock(ticker, period)
+def create_tab(ticker, period, percentage):
+    # Retrieve stock data and predictions. Validate stock data.
+    try:
+        stock_data = yf.Ticker(ticker).history(period=str(period))
+    except ValueError as e:
+        messagebox.showerror("Error", str(e))
+        return
 
     # Add buying and selling points to stock data
     stock_data['Buying Point'] = stock_data['Close'].shift(1) * (1 + percentage/100)
@@ -54,12 +64,10 @@ def create_tab(ticker, percentage, period):
     canvas = FigureCanvasTkAgg(figure, tab)
     canvas.draw()
     canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
-
 # Create main window
 window = tk.Tk()
 window.title("Stock Predictor")
-window.geometry("600x400") # Make this window resizeable, minamizeable, and fullscreen capeable.
-
+window.geometry("600x400") # Make this window resizable,  minimize, and fullscreen capable.
 # Create tab control
 tab_control = ttk.Notebook(window)
 tab_control.pack(expand=1, fill='both')
@@ -68,42 +76,61 @@ tab_control.pack(expand=1, fill='both')
 input_frame = Frame(window)
 input_frame.pack(side='top')
 
+# Create prediction frame
+prediction_frame = Frame(window)
+prediction_frame.pack(side='top')
+
+# Create buy frame
+buy_frame = Frame(window)
+buy_frame.pack(side='top')
+
+# Create sell frame
+sell_frame = Frame(window)
+sell_frame.pack(side='top')
+
+# Create buy button
+buy_button = Button(buy_frame, text='Buy', command=lambda: buy_frame.destroy())
+buy_button.pack()
+
+# Create sell button
+sell_button = Button(sell_frame, text='Sell', command=lambda: sell_frame.destroy())
+sell_button.pack()
+
+# Create prediction button
+prediction_button = Button(prediction_frame, text='Predict', command=lambda: prediction_frame.destroy())
+prediction_button.pack()
+
+#Create period label and entry
+period = StringVar()
+period_label = tk.Label(window, text="Time Period : #y : #mo : ytd :max")
+period_entry = tk.Entry(window)
+period.set('1Y') # Default
+period_label.pack()
+period_entry.pack()
+
+# Create percentage label and entry
+percentage = StringVar()
+percentage_label = tk.Label(input_frame, text="Percentage:")
+percentage_entry = tk.Entry(input_frame, textvariable=percentage)
+percentage.set('0.1') # Default
+percentage_label.pack(side='left')
+percentage_entry.pack(side='left')
+
 # Create ticker label and entry
 ticker_label = tk.Label(input_frame, text="Ticker:")
 ticker_label.pack(side='left')
 ticker_entry = tk.Entry(input_frame)
 ticker_entry.pack(side='left')
 
-# Create percentage label and entry
-
-percentage_label = tk.Label(window, text="Percentage:")
-percentage_entry = tk.Entry(window)
-if percentage_entry is None:
-    percentage_entry = 0.01 # Default?
-percentage_label.pack()
-percentage_entry.pack()
-
-#Create period label and entry
-
-period_label = tk.Label(window, text="Time Period : YTD")
-period_entry = tk.Entry(window)
-if period_entry is None:
-    period_entry = '1Y' # 1 YTD Default
-period_label.pack()
-period_entry.pack()
-
 #Create add button
-
-add_button = tk.Button(window, text="Add", command=lambda: create_tab(ticker_entry.get(), float(percentage_entry.get()), period_entry.get()))
+add_button = tk.Button(window, text="Add", command=lambda: create_tab(ticker_entry.get(), period_entry.get(), float(percentage_entry.get())))
 add_button.pack()
 
 #Create quit button
-
 quit_button = tk.Button(window, text="Quit", command=window.quit)
 quit_button.pack()
 
 #Run Tkinter event loop
-
 window.mainloop()
 
 
