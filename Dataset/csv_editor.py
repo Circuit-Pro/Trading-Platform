55aa469faf40698a7bb455241d27ecab1daf4b2b
CPI <oricalworks@gmail.com>
2023-01-06 10:25:22 -0500

    fixes

--- Dataset/csv_editor.py
+++ Dataset/csv_editor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# This script converts data sets to the properly excpected format.
+# This script converts data sets to the properly expected format.
 # converts 2007-01-01 00:00:00 to 18.10.2019 00:00:00.000 GMT+0300
 from datetime import datetime, timezone
 import csv
@@ -9,7 +9,7 @@ import os
 input_file = os.path.join(os.getcwd(), 'Trading-Platform', 'Dataset', 'new_set.csv')
 output_file = os.path.join(os.getcwd(), 'Trading-Platform', 'Dataset', 'set.csv')
 
-with open(input_file, 'r') as f_input, open(output_file, 'w') as f_output:
+with open(input_file, 'r') as f_input, open(output_file, 'w', newline='') as f_output:
     csv_input = csv.reader(f_input)
     csv_output = csv.writer(f_output)
     next(csv_input)  # skip header line
@@ -19,16 +19,13 @@ with open(input_file, 'r') as f_input, open(output_file, 'w') as f_output:
             dt = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
         except ValueError:
             # if parsing fails, assume the date is already in the correct format
-            print("Row Has Correct Format:",row, "\n")
+            print("Row Has Correct Format:", row, "\n")
+            csv_output.writerow(row)
             continue
-        tz = dt.tzinfo  # get the timezone information
-        if tz is not None:
-            # format the timezone offset as +HHMM or -HHMM
-            tz_offset = tz.utcoffset(dt).total_seconds() // 60
-            tz_offset_str = '{:+05d}'.format(int(tz_offset))
-        else:
-            tz_offset_str = '+0000'  # default to UTC if no timezone information is available
-        row[0] = dt.strftime('%d.%m.%Y %H:%M:%S.000 GMT' + tz_offset_str)  # assign the revised format
+        tz = timezone(timedelta(hours=3))  # create a timezone object for GMT+0300
+        dt = dt.replace(tzinfo=tz)  # assign the timezone to the datetime object
+        row[0] = dt.strftime('%d.%m.%Y %H:%M:%S.000 GMT+0300')  # assign the revised format
         csv_output.writerow(row)
-        print("Successfully Formatted Row:",row, "\n")
+        print("Successfully Formatted Row:", row, "\n")
     print("Success!")
+
