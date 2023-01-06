8cdc14ee75e4c95fec6b6398df0a9925eeba36fa
CPI <oricalworks@gmail.com>
2023-01-06 12:35:08 -0500

    Update - working!

--- Dataset/csv_editor.py
+++ Dataset/csv_editor.py
@@ -18,20 +18,20 @@ with open(input_file, 'r') as f_input, open(output_file, 'w', newline='') as f_o
     f_input.seek(0)
     total = len(pd.read_csv(f_input))
     f_input.seek(0)
-    for row in tqdm(csv_input, total=total, unit="rows"):
+    for i, row in enumerate(tqdm(csv_input, total=total, unit="row")):
         date_time = row[0]  # extract the date and time value
         try:
-            # try to parse the date as the original format
+        # try to parse the date as the original format
             dt = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
         except ValueError:
-            # if parsing fails, assume the date is already in the correct format
+        # if parsing fails, assume the date is already in the correct format
             csv_output.writerow(row)
-            print("Row Has Correct Format:", row, "Current : Total", row+1,":", total, "\n")
+            print("Row Has Correct Format:", row, "Current : Total", i+1,":", total, "\n")
             continue
         tz = timezone(timedelta(hours=3))  # create a timezone object for GMT+0300
         dt = dt.replace(tzinfo=tz)  # assign the timezone to the datetime object
         date_time = dt.strftime('%d.%m.%Y %H:%M:%S.000 GMT+0300')  # assign the revised format
         row[0] = date_time  # update the date and time value in the row
         csv_output.writerow(row)  # write the row to the output file
-        print("Successfully Formatted Row:", row, "Current : Total", row+1,":", total, "\n")
+        #print("Successfully Formatted Row:", row, "Current : Total", i+1,":", total, "\n")
     print("Success!")
